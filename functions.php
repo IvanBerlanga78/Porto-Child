aed411934ae1513596ec208f1c75ecbe02bc3964
IvanBerlanga78 <iberlanga78@gmail.com>
2020-01-31 17:39:31 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -81,7 +81,6 @@ function mytheme_add_woocommerce_support() {
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 
-
 /**
  * @snippet       Shipping by Weight | WooCommerce
  */
